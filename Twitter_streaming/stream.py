6da0acb84382f6383601191821de9d9c88a3a810
andyzhao1 <andyshengzhe@hotmail.com>
2020-05-13 19:00:09 +1000

    changed

---
+++ Twitter_streaming/stream.py
@@ -0,0 +1,66 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import couchdb
+import json
+
+class myListener(StreamListener):
+
+    def on_data(self, data):
+        # When receiving data from twitter will call this method
+        try:
+            # print(data)
+            db = get_db(server, "Twitter")  # get db
+            data_json = json.loads(data)  # Decode the JSON from Twitter
+
+            db.update(data_json)  # insert the data into the couchdb into a collection
+        except Exception as e:
+            print(e)
+
+    def on_error(self, status):
+        print("Error: " + str(status))
+
+    def on_connect(self):
+        print("You are now connected to the streaming API.")
+
+
+def server_conn():
+    try:
+        server = couchdb.Server('http://COMP90024:COMP90024@172.26.131.52:5984/')
+        return server
+    except Exception as e:
+        print(e)
+		
+
+def get_db(server, db_name):
+    try:
+        db = server[db_name]
+        return db
+    except Exception as e:
+        print(e)
+
+
+def update_doc(db, docs):
+    db.update(docs)
+
+
+# Twitter API key
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+
+
+server = server_conn()
+
+# db = create_db(server, "Twiiter")
+# Twiiter = get_db(server, 'Twiiter')
+
+
+listener = myListener()
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+stream = Stream(auth, listener)
+stream.filter(track=['test'], is_async=True)
+#res = stream.filter(track=['test'], is_async=True)
