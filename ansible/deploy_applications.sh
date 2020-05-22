ccc8b38de48253ea387551d30f5ae23ecaaadbf9
zkzzzkz <zhijunh1@student.unimelb.edu.au>
2020-05-23 04:45:57 +1000

    playbook for application deployment

--- ansible/deploy_applications.sh
+++ ansible/deploy_applications.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-. ./openrc.sh; ansible-playbook -i inventory/hosts.ini --ask-become-pass lanuch_nectar.yaml
+. ./openrc.sh; ansible-playbook -i inventory/hosts.ini --ask-become-pass deploy_applications.yaml
--- ansible/lanuch_nectar.sh
+++ ansible/lanuch_nectar.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
 . ./openrc.sh; ansible-playbook -i hosts --ask-become-pass lanuch_nectar.yaml
